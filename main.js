6e2b96b0a550a175ebf5677443cf29e60e77c8bc
Diana Cristea <dicristea111@gmail.com>
2022-07-27 21:31:52 -0700

    Finish main styling and password match

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-// when .required-list>input is invalid, add error content to that list's .error-box
-
-// also add red to outline
 
 const submit = document.getElementById('submit-button');
+const error = document.querySelector('#error-message');
 
+submit.addEventListener('mousedown', function() {
+    const userPassword = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    console.log(userPassword);
+    console.log(confirmPassword);
 
-submit.addEventListener('click', function() {
-    submit.style.boxShadow = ' ';
+    submit.style.boxShadow = "0px 0px";
+    checkPasswordMatch(userPassword, confirmPassword);
 });
+
+submit.addEventListener('mouseup', function() {
+    submit.style.boxShadow = "3px 5px 5px rgba(128, 128, 128, 0.228)";
+});
+
+function checkPasswordMatch(userPassword, confirmPassword) {
+    if(confirmPassword !== userPassword) {        
+        error.style.padding = '0px 0px';
+        error.textContent = "* Passwords do not match"
+        const inputBorder = Array.from(document.querySelectorAll('.required-list input'));
+        console.log(inputBorder);
+        for(let i = 0; i < inputBorder.length; i++) {
+            inputBorder[i].style.border = '1px solid rgb(238, 0, 0';
+        }
+    }
+};
